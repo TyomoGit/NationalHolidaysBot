9840c57fcd4ad178d6e4191d25872f8b3b72fe80
TyomoGit <riku3dno.1@gmail.com>
2022-11-02 00:56:36 +0900

    月初めに祝日をついイートする機能を追加

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 """ for Twitter API v2 """
 
-from datetime import datetime
 from email import message
-from http import client
+import jpholiday as jph
+from datetime import datetime
 from pprint import pprint
 import tweepy
 import keys
 
-client = tweepy.Client(
-    consumer_key=keys.API_KEY,
-    consumer_secret=keys.API_SECRET,
-    access_token=keys.ACCESS_TOKEN,
-    access_token_secret=keys.ACCESS_TOKEN_SECRET
-)
+def tweet(message: str = str(datetime.now())) -> None:
+    client = tweepy.Client(
+        consumer_key=keys.API_KEY,
+        consumer_secret=keys.API_SECRET,
+        access_token=keys.ACCESS_TOKEN,
+        access_token_secret=keys.ACCESS_TOKEN_SECRET
+    )
+    pprint(client.create_tweet(text=message))
 
-# message = str(datetime.now())
-message = input("tweet: ")
-if message == "":
-    message = str(datetime.now())
+def main():
+    today = datetime.now().date()
 
+    if jph.is_holiday(today):
+        holiday_name = jph.is_holiday_name(today)
+        message = f"今日は{holiday_name}です。"
+        # tweet(message)
+    
+    if today.day == 1:
+        message = f"{today.month}月になりましたね。"
+        message_holidays = ""
+        holidays = jph.month_holidays(today.year, today.month)
+        counter = 0
 
-pprint(client.create_tweet(text=message))
+        for holiday in holidays:
+            counter += 1
+            for content in holiday:
+                if type(content) is datetime:
+                    message_holidays += str(content) + ": "
+                elif type(content) is str:
+                    message_holidays += content
+            message_holidays += "\n"
+        
+        message += f"祝日は{counter}日あります:\n" + message_holidays
+        tweet(message)
+
+if __name__ == "__main__":
+    main()
