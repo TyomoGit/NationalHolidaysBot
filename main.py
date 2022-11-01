81b04e6d16a31ab94cdcc12da22530db03e359b4
TyomoGit <riku3dno.1@gmail.com>
2022-11-02 01:49:20 +0900

    リファクタリング

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 from email import message
 import jpholiday as jph
-from datetime import datetime
+from datetime import date, datetime
+import sys
 from pprint import pprint
 import tweepy
 import keys
@@ -14,33 +15,42 @@ def tweet(message: str = str(datetime.now())) -> None:
         access_token=keys.ACCESS_TOKEN,
         access_token_secret=keys.ACCESS_TOKEN_SECRET
     )
+    if len(sys.argv) > 1:
+        message = "これは試験的なツイートです。以下の内容は正確でない場合があります。\n" + message
+
     pprint(client.create_tweet(text=message))
 
-def main():
-    today = datetime.now().date()
+def behavior_begining_of_month(today: datetime) -> None:
+    message = f"{today.month}月になりましたね。"
+    message_holidays = ""
+    holidays = jph.month_holidays(today.year, today.month)
+    counter = 0
 
-    if jph.is_holiday(today):
-        holiday_name = jph.is_holiday_name(today)
-        message = f"今日は{holiday_name}です。"
-        # tweet(message)
+    for holiday in holidays:
+        counter += 1
+        for content in holiday:
+            if type(content) is date:
+                message_holidays += str(content) + ": "
+            elif type(content) is str:
+                message_holidays += content
+        message_holidays += "\n"
+    
+    message += f"祝日は{counter}日あります:\n" + message_holidays.replace(f"{str(today.year)}-", "").replace("-", "/")
+    tweet(message)
+
+def behavior_first(today: datetime) -> None:
+    holiday_name = jph.is_holiday_name(today)
+    message = f"🗓今日は{holiday_name}です。"
+    tweet(message)
+
+def main():
+    # today = datetime.now().date()
+    today = datetime(2022,3,1).date()
     
     if today.day == 1:
-        message = f"{today.month}月になりましたね。"
-        message_holidays = ""
-        holidays = jph.month_holidays(today.year, today.month)
-        counter = 0
-
-        for holiday in holidays:
-            counter += 1
-            for content in holiday:
-                if type(content) is datetime:
-                    message_holidays += str(content) + ": "
-                elif type(content) is str:
-                    message_holidays += content
-            message_holidays += "\n"
-        
-        message += f"祝日は{counter}日あります:\n" + message_holidays
-        tweet(message)
+        behavior_begining_of_month(today)
+    if jph.is_holiday(today):
+        behavior_first(today)
 
 if __name__ == "__main__":
     main()
